218d939dc2ec25043d061882e6cc6b6a9ecaaa8c
Swazil <33392953+Swazil@users.noreply.github.com>
2017-11-15 14:06:49 +0500

    Initial commit

---
+++ Assets/ButtonManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class ButtonManager : MonoBehaviour 
+{
+	public void NewGameBtn(string newGameLevel)
+	{
+		SceneManager.LoadScene (newGameLevel);
+	}
+
+	public void ExitGameBtn()
+	{
+		Application.Quit ();
+	}
+
+}
